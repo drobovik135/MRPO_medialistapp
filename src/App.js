587a75dc3cdfec15676f09c831ccc8554a4d9e8a
Thaumiel0 <drobovik135@gmail.com>
2024-12-20 02:05:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import './index.css';
+import EntryMedia from './components/EntryMedia';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movie List</h1>
+      <EntryMedia />
+    </div>
+  );
+}
+
+export default App;
