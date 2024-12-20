cf2156696ce79a338583006804928ee2197b6229
drobash <drobovik135@gmail.com>
2024-12-20 15:21:21 +0300

    Push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className="header">
-    <div className="header-content">
-      <Link to='/'>
-        <h1 className="header-title">My Media List</h1>
-      </Link>
-    </div>
-  </header>
+      
+        <div className="header-content">
+        <Link className='link-Header' to ="/">
+          <h1 className="header-title">My Media List</h1>
+          </Link>
+        </div>
+
+    </header>
   )
 }
 
