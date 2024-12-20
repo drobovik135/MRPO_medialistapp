2f8d5b38f051c36124f584fd9dbf7578723b9b21
drobash <drobovik135@gmail.com>
2024-12-20 15:13:18 +0300

    haeder

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className="header">
     <div className="header-content">
-      <h1 className="header-title">My Media List</h1>
+      <Link to='/'>
+        <h1 className="header-title">My Media List</h1>
+      </Link>
     </div>
   </header>
   )
