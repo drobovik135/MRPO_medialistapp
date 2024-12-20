faea48634b8dff56381aa3b8cacab722ca5fea94
drobash <drobovik135@gmail.com>
2024-12-20 07:44:58 +0300

    Pages
    Cock

--- src/components/AllMedia.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AllMedia = () => {
-  return (
-    <div>AllMedia</div>
-  )
-}
-
-export default AllMedia
---
+++ src/pages/EntryMediaPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const EntryMediaPage = (user, list) => {
+    return (
+        <Link to={`/entries/${entryMedia.id}`}>
+            <div class="media-entry">
+                <div class="media-poster">
+                    <img src={entryMedia.imgUrl} />
+                </div>
+                <div class="media-info">
+                    <h2 class="media-title">{entryMedia.name}</h2>
+                    <p class="mediaEntry-rating">Your Rating: {entryMedia.mediaRate}</p>
+                    <p class="media-genre">Genre: {entryMedia.genre}</p>
+                </div>
+            </div>
+        </Link>
+    );
+}
+
+export default EntryMediaPage;
