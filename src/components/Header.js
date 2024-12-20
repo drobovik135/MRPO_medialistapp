763381934926952cc5e1cb0486f137dfb10d5a8c
drobash <drobovik135@gmail.com>
2024-12-20 08:32:46 +0300

    Cool

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => {
     <header className="header">
     <div className="header-content">
       <h1 className="header-title">My Media List</h1>
-        <Link to= {`/allmedia`} className="header-link"> View Media List</Link>
     </div>
   </header>
   )
