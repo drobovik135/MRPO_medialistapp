a97b0c8fcabe662abeec41342d1a92af6d07b154
Drobovik <drobovik135@gmail.com>
2024-12-20 09:23:53 +0300

    upd

--- src/components/AllMedia.js
+++ src/components/AllMedia.js
@@ -8,31 +8,52 @@ export const AllMedia = () => {
             this.state = {
                 error: null,
                 isLoaded: false,
+                category: {},
                 medias: []
             }
         }
     
         componentDidMount(){
-            fetch('http://localhost:8080/medias')
-              .then(res => res.json())
-              .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        medias: result
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                });
+            const queryParameters = new URLSearchParams(window.location.search);
+            const categoryId = queryParameters.get("categoryId");
+
+            
+            fetch('http://localhost:8080/categories/'+categoryId)
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.setState({
+                            isLoaded: true,
+                            category: result
+                        });
+                    },
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+            });
+                
+            fetch('http://localhost:8080/categories/'+categoryId+"/media")
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.setState({
+                            isLoaded: true,
+                            medias: result
+                        });
+                    },
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+            });
         }
     
         render() {
-            const { error, isLoaded, medias } = this.state;
-            return UserListPage(medias);
+            const { error, isLoaded, category, medias } = this.state;
+            return UserListPage(category, medias);
         }
     };
     
